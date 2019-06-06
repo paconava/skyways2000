0cb64224dde54dd27ce9822ca192ba168c6eb221
Paco Nava <paconava@comunidad.unam.mx>
2019-06-06 05:20:03 -0500

    El resto

--- ProyectoFinal/build/Debug/GNU-Linux-x86/nbproject/src/main.o.d
+++ ProyectoFinal/build/Debug/GNU-Linux-x86/nbproject/src/main.o.d
@@ -1,16 +1,4 @@
 build/Debug/GNU-Linux-x86/nbproject/src/main.o: nbproject/src/main.cpp \
- /usr/include/freetype2/ft2build.h \
- /usr/include/freetype2/freetype/config/ftheader.h \
- /usr/include/freetype2/freetype/freetype.h \
- /usr/include/freetype2/freetype/config/ftconfig.h \
- /usr/include/freetype2/freetype/config/ftoption.h \
- /usr/include/freetype2/freetype/config/ftstdlib.h \
- /usr/include/freetype2/freetype/fttypes.h \
- /usr/include/freetype2/freetype/ftsystem.h \
- /usr/include/freetype2/freetype/ftimage.h \
- /usr/include/freetype2/freetype/fterrors.h \
- /usr/include/freetype2/freetype/ftmoderr.h \
- /usr/include/freetype2/freetype/fterrdef.h \
  nbproject/external/glfw/include/GLFW/glfw3.h \
  nbproject/src/Headers/InputManager.h nbproject/src/Headers/TimeManager.h \
  nbproject/src/Headers/Shader.h nbproject/src/Headers/Texture.h \
@@ -20,30 +8,6 @@ build/Debug/GNU-Linux-x86/nbproject/src/main.o: nbproject/src/main.cpp \
  nbproject/src/Headers/CubemapTexture.h nbproject/src/Headers/Model.h \
  nbproject/src/Headers/Mesh.h nbproject/src/Headers/Sphere.h \
  nbproject/src/Headers/collision.h
-
-/usr/include/freetype2/ft2build.h:
-
-/usr/include/freetype2/freetype/config/ftheader.h:
-
-/usr/include/freetype2/freetype/freetype.h:
-
-/usr/include/freetype2/freetype/config/ftconfig.h:
-
-/usr/include/freetype2/freetype/config/ftoption.h:
-
-/usr/include/freetype2/freetype/config/ftstdlib.h:
-
-/usr/include/freetype2/freetype/fttypes.h:
-
-/usr/include/freetype2/freetype/ftsystem.h:
-
-/usr/include/freetype2/freetype/ftimage.h:
-
-/usr/include/freetype2/freetype/fterrors.h:
-
-/usr/include/freetype2/freetype/ftmoderr.h:
-
-/usr/include/freetype2/freetype/fterrdef.h:
 
 nbproject/external/glfw/include/GLFW/glfw3.h:
 
